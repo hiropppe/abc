24e278fafdc53b04dcd76b91ec26f48b59dcb957
hiropppe <sergei.0910@gmail.com>
2019-12-05 11:56:32 +0900

    trial data

---
+++ training-scripts/zipporah/prepare_good_dev.sh
@@ -0,0 +1,8 @@
+python split_good_dev.py /data/bitextor/corpus/QED en ja 500
+python split_good_dev.py /data/bitextor/corpus/Tatoeba en ja 500
+python split_good_dev.py /data/bitextor/corpus/TED en ja 500
+python split_good_dev.py /data/bitextor/corpus/KFTT en ja 500
+python split_good_dev.py /data/bitextor/corpus/OpenSubtitles en ja 500
+python split_good_dev.py /data/bitextor/corpus/GlobalVoices en ja 500
+python split_good_dev.py /data/bitextor/corpus/JESC en ja 500
+
